f5da4fc10fefc1a99c7c816a746da79b9b12fb4a
Andrew Reid East <andrew@andreweast.net>
2017-09-21 15:51:41 +0100

    Question 1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -2,5 +2,19 @@
 
 int main(int arg, char* argc[]){
   printf("Hello assignment1.\n");
+
+  int i = 42;
+  int* iPtr = &i;
+  long l = 9000;
+//  double d = 3.14159;
+  double* dPtr;
+  char** strings;
+
+  printf("i %lu\n", sizeof(i));
+  printf("iPtr %lu\n", sizeof(iPtr));
+  printf("l %lu\n", sizeof(l));
+  printf("dPtr %lu\n", sizeof(dPtr));
+  printf("strings %lu\n", sizeof(strings));
+
   return 0;
 }
